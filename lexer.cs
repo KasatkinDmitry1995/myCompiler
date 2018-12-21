771d7163de8190b60c9715c2c7a998096e42b707
KasatkinDmitry1995 <45930319+KasatkinDmitry1995@users.noreply.github.com>
2018-12-21 16:07:49 +0300

    Update lexer.cs

--- lexer.cs
+++ lexer.cs
@@ -1,4 +1,3 @@
-
 /*
  * Created by SharpDevelop.
  * User: Касаткин
@@ -7,27 +6,42 @@
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
+
 using System;
 using System.Collections.Generic;
 using System.Text;
 
+
+
+
 namespace lexer
 {
+	
+	
     public enum TokType
     {
         NUMBER = 0, IDENT, ADD, SUB, MUL, DIV, MOD, NEG, SHL, SHR, ASSIGN,
-        CONDL, CONDG, CONDLE, CONDGE, CONDE, CONDNE, IF, ELSE, RFBRACE, LFBRACE, RRBRACE, LRBRACE,
-        DIRECTIVE
+        CONDL, CONDG, CONDLE, CONDGE, CONDE, CONDNE, 
+        IF, ELSE, WHILE, DO, SWITCH, FOR, CASE, BREAK, CONTINUE, PROCEDURE, FUNCTION,
+        RFBRACE, LFBRACE, RRBRACE, LRBRACE, SEMICOLON, COMMA
     }
 
     public abstract class Token
     {
         protected TokType tokenType;
+        protected Int32 line;
         public TokType TokenType
         {
             get
             {
                 return tokenType;
+            }
+        }
+        public Int32 Line
+        {
+            get
+            {
+                return line;
             }
         }
         public bool IsTokOfType(TokType _tokenType)
@@ -45,18 +59,41 @@ namespace lexer
             return tokenType <= TokType.IDENT;
         }
         public abstract override String ToString();
+        
+        public static readonly Dictionary<String, TokType> Directives = new Dictionary<string, TokType>()
+		{
+			{@"if",TokType.IF},
+			{@"else",TokType.ELSE},
+			{@"while",TokType.WHILE},
+			{@"do",TokType.DO},
+			{@"for",TokType.FOR},
+			{@"switch",TokType.SWITCH},
+			{@"case",TokType.CASE},
+			{@"break",TokType.BREAK},
+			{@"continue",TokType.CONTINUE},
+			{@"procedure",TokType.PROCEDURE},
+			{@"function",TokType.FUNCTION}
+		};
+        
+        public static Token GetVarOrDirective(String str, Int32 line){
+        	if(Directives.ContainsKey(str)){
+        		return new SimpleToken(Directives[str], line);
+        	}
+        	return new IdentToken(str, line);
+        }
     }
 
     public class SimpleToken : Token
     {
-        public SimpleToken(TokType _tokenType)
+        public SimpleToken(TokType _tokenType, Int32 _line)
         {
             tokenType = _tokenType;
+            line = _line;
         }
 
         public override String ToString()
         {
-            return @"[" + tokenType.ToString() + @"]";
+        	return line.ToString() +@" >> [" + tokenType.ToString() + @"]";
         }
     }
 
@@ -65,15 +102,13 @@ namespace lexer
         private Int64 number;
         public Int64 Number
         {
-            get
-            {
-                return number;
-            }
+            get { return number; }
         }
 
-        public NumberToken(String str, Byte numBase)
+        public NumberToken(String str, Byte numBase, Int32 _line)
         {
             tokenType = TokType.NUMBER;
+            line = _line;
             switch (numBase)
             {
                 case 2:
@@ -93,7 +128,7 @@ namespace lexer
 
         public override String ToString()
         {
-            return @"[" + tokenType.ToString() + "] = \"" + number.ToString() + "\"";
+            return line.ToString() +@" >> [" + tokenType.ToString() + "] = \"" + number.ToString() + "\"";
         }
 
     }
@@ -103,202 +138,204 @@ namespace lexer
         private String ident;
         public string Ident
         {
-            get
-            {
-                return ident;
-            }
+            get {  return ident; }
         }
 
-        public IdentToken(String _ident)
+        public IdentToken(String _ident, Int32 _line)
         {
             ident = _ident;
+            line = _line;
             tokenType = TokType.IDENT;
         }
 
         public override String ToString()
         {
-            return @"[" + tokenType.ToString() + "] = \"" + ident + "\"";
+            return line.ToString() +@" >> [" + tokenType.ToString() + "] = \"" + ident + "\"";
         }
     }
 
     public class Program
     {
         private static List<Token> commandList = new List<Token>();
-        private static StringBuilder tmp;
-        private static Int32 i;
-
-
+        private static List<String> errorList = new List<string>();
+        
         public static void Main(string[] args)
         {
-            string s = "a=5-(0b10010+c)*b+(a!=10)\n" +
-                "//gbfgbfgbfg gbft b fgb fgb f\n" +
-                "gbfgb + g\n";
+            string s = "a?=5;\n" +
+                "?a =  b-3?;//gbfgbfgbfg gbft b fgb fgb f\n" +
+                "?  \n" +
+            	"gbfgb + g;\n" +
+            	"function func(a, b, c)\n" +
+            	"{\n" +
+            	"	return a + b + c;\n" +
+            	"}\n" +
+            	"//comment\n" +
+            	"func(3,2,8)\n" +
+            	"\n" +
+            	"";
 
             ListFromString(s);
 
             foreach (Token token in commandList)
                 Console.WriteLine(token);
+            foreach (String str in errorList)
+            	Console.WriteLine(str);
             Console.WriteLine("Press any key to continue . . . ");
             Console.ReadKey(true);
 
         }
 
-        private static bool IsDelimeter(char ch)
-        {
-            return ("<>=(){}*/+-~%;!".IndexOf(ch) != -1) || char.IsWhiteSpace(ch);
-        }
-
-
         private static void ListFromString(String code)
         {
+        	
             StringBuilder tmp = new StringBuilder();
-            code.Replace('\t', ' ');
-            i = 0;
-            int _ = 5;
+            Int32 line = 1, i = 0, last_i = -1;
             Byte numberBase;
-            try
+
+            while (i < code.Length)
             {
-                while (i < code.Length)
+                // Нашли букву - считываем идентификатор в буфер
+                if (char.IsLetter(code, i) || code[i] == '_')
                 {
-                    // Нашли букву - считываем идентификатор в буфер
-                    if (char.IsLetter(code, i) || code[i] == '_')
-                    {
+                    tmp.Append(code[i++]);
+                    while (i < code.Length && (char.IsDigit(code, i) || char.IsLetter(code, i) || code[i] == '_'))
                         tmp.Append(code[i++]);
-                        while (i < code.Length && (char.IsDigit(code, i) || char.IsLetter(code, i) || code[i] == '_'))
-                            tmp.Append(code[i++]);
-                        if (tmp.Length > 0)
+
+                    commandList.Add(Token.GetVarOrDirective(tmp.ToString(), line));
+                    tmp.Length = 0; // очищаем буфер
+                }else if (char.IsDigit(code, i)) 
+                {
+                        if (i + 1 < code.Length & code[i] == '0')
                         {
-                            commandList.Add(new IdentToken(tmp.ToString()));
-                            tmp.Length = 0; // И очищаем буфер
-                        }
-                    }  else if (char.IsDigit(code, i))
-                    {
-                            if (i + 1 < code.Length & code[i] == '0')
+                            if (code[++i] == 'x')
                             {
-                                if (code[++i] == 'x')
-                                {
-                                    ++i;
-                                    numberBase = 16;
-                                    while (char.IsDigit(code, i) && char.ToUpper(code[i]) > 'A' && char.ToUpper(code[i]) <= 'F')
-                                        tmp.Append(code[i++]);
-                                }
-                                else if (code[i] == 'b')
-                                {
-                                    ++i;
-                                    numberBase = 2;
-                                    while (i < code.Length && code[i] == '0' && code[i] == '1')
-                                        tmp.Append(code[i++]);
-                                }
-                                else
-                                {
-                                    ++i;
-                                    numberBase = 8;
-                                    while (i < code.Length && code[i] > '0' && code[i] <= '7')
-                                        tmp.Append(code[i++]);
-                                }
-                            } else {
-                                numberBase = 10;
-                                while (char.IsDigit(code, i))
+                                ++i;
+                                numberBase = 16;
+                                while (char.IsDigit(code, i) && char.ToUpper(code[i]) > 'A' && char.ToUpper(code[i]) <= 'F')
+                                    tmp.Append(code[i++]);
+                            } else if (code[i] == 'b')
+                            {
+                                ++i;
+                                numberBase = 2;
+                                while (i < code.Length && code[i] == '0' && code[i] == '1')
+                                    tmp.Append(code[i++]);
+                            } else
+                            {
+                                ++i;
+                                numberBase = 8;
+                                while (i < code.Length && code[i] > '0' && code[i] <= '7')
                                     tmp.Append(code[i++]);
                             }
-
-                            if (tmp.Length > 0 && IsDelimeter(code[i]))
-                            {
-                                commandList.Add(new NumberToken(tmp.ToString(), numberBase));
-                                tmp.Length = 0;
-                            }
-
-                    } else switch (code[i]) {
+                        } else {
+                            numberBase = 10;
+                            while (char.IsDigit(code, i))
+                                tmp.Append(code[i++]);  
+                        }
+                        commandList.Add(new NumberToken(tmp.ToString(), numberBase, line));
+                        tmp.Length = 0;
+                        
+                } else {
+                	switch (code[i]) {
+                    	
                         case '+':
-                            commandList.Add(new SimpleToken(TokType.ADD));
+                            commandList.Add(new SimpleToken(TokType.ADD, line));
                             break;
                         case '-':
-                            commandList.Add(new SimpleToken(TokType.SUB));
+                            commandList.Add(new SimpleToken(TokType.SUB, line));
                             break;
                         case '*':
-                            commandList.Add(new SimpleToken(TokType.MUL));
-                            break;
-                        case '/':
-                            commandList.Add(new SimpleToken(TokType.DIV));
+                            commandList.Add(new SimpleToken(TokType.MUL, line));
                             break;
                         case '%':
-                            commandList.Add(new SimpleToken(TokType.MOD));
+                            commandList.Add(new SimpleToken(TokType.MOD, line));
                             break;
                         case '~':
-                            commandList.Add(new SimpleToken(TokType.NEG));
+                            commandList.Add(new SimpleToken(TokType.NEG, line));
                             break;
                         case '{':
-                            commandList.Add(new SimpleToken(TokType.LFBRACE));
+                            commandList.Add(new SimpleToken(TokType.LFBRACE, line));
                             break;
                         case '(':
-                            commandList.Add(new SimpleToken(TokType.LRBRACE));
+                            commandList.Add(new SimpleToken(TokType.LRBRACE, line));
                             break;
                         case '}':
-                                commandList.Add(new SimpleToken(TokType.RFBRACE));
+                            commandList.Add(new SimpleToken(TokType.RFBRACE, line));
                             break;
                         case ')':
-                                commandList.Add(new SimpleToken(TokType.RRBRACE));
+                            commandList.Add(new SimpleToken(TokType.RRBRACE, line));
+                            break;
+                        case ',':
+                            commandList.Add(new SimpleToken(TokType.COMMA, line));
                             break;
                         case '>':
-                            if (++i < code.Length & code[i] == '=')
+                            if (i+1 < code.Length & code[i+1] == '=')
                             {
-                                commandList.Add(new SimpleToken(TokType.CONDGE));
-                                ++i;
+                                commandList.Add(new SimpleToken(TokType.CONDGE, line));
+								++i;
                             } else
-                                commandList.Add(new SimpleToken(TokType.CONDG));
+                                commandList.Add(new SimpleToken(TokType.CONDG, line));
                             break;
                         case '<':
-                            if (++i < code.Length & code[i] == '=')
+                            if (i+1 < code.Length & code[i+1] == '=')
                             {
-                                commandList.Add(new SimpleToken(TokType.CONDLE));
+                                commandList.Add(new SimpleToken(TokType.CONDLE, line));
                                 ++i;
                             } else  
-                                commandList.Add(new SimpleToken(TokType.CONDL));
+                                commandList.Add(new SimpleToken(TokType.CONDL, line));
                             break;
                         case '!':
-                            if (++i < code.Length & code[i] == '=')
+                            if (i+1 < code.Length & code[i+1] == '=')
                             {
-                                commandList.Add(new SimpleToken(TokType.CONDNE));
+                                commandList.Add(new SimpleToken(TokType.CONDNE, line));
                                 ++i;
                             } else 
-                                commandList.Add(new SimpleToken(TokType.NEG));
+                                commandList.Add(new SimpleToken(TokType.NEG, line));
                             break;
                         case '=':
-                            if (++i < code.Length & code[i] == '=')
+                            if (i+1 < code.Length & code[i+1] == '=')
                             {
-                                commandList.Add(new SimpleToken(TokType.CONDE));
+                                commandList.Add(new SimpleToken(TokType.CONDE, line));
                                 ++i;
-                            } else
-                                commandList.Add(new SimpleToken(TokType.ASSIGN));
+                            } else 
+                                commandList.Add(new SimpleToken(TokType.ASSIGN, line));
                             break;
+                        case '/':
+                         	if (i+1 < code.Length & code[i+1] == '/')
+                            {
+                         		i = code.IndexOf('\n', i);
+                         		if(i == -1){
+                         			i = code.Length;
+                         			continue;
+                         		}else {
+                         			++line;
+                         			last_i = i;
+                         		}
+                            } else 
+                                commandList.Add(new SimpleToken(TokType.DIV, line));
+                            break; 
+                        case ';':
+                            commandList.Add(new SimpleToken(TokType.SEMICOLON, line));
+                            break;
+                        case ' ':
+                        case '\r':
+                        case '\t':
+                            while ((++i < code.Length) & 
+                                   (code[i] == ' ' || code[i] == '\r' || code[i] == '\t')) 
+                            	{ }
+                            continue;
+                        case '\n':
+                        	++line;
+                        	last_i = i;
+                        	break;
                         default:
-                                if (IsDelimeter(code[i]))
-                                {
-                                    if (code.Length >= i+1 && code[i] == '/' && code[i+1] == '/') // пропускаем комментарии
-                                        i = code.IndexOf('\n', i) + 1;
-                                    else
-                                    {
-                                        commandList.Add(new SimpleToken(TokType.ADD));
-                                        ++i;
-                                    }
-                                }
-                                else if (Char.IsWhiteSpace(code[i]))
-                                    while ((++i < code.Length) & Char.IsWhiteSpace(code[i])) { }
-                                else
-                                    throw new Exception("Какаято херня у вас происходит.....");
-                                break;
-
+                        	errorList.Add("Неожиданный символ в исходном тексте программы, на строке " + 
+                        					line.ToString() + " на позиции " + (i - last_i).ToString());
+                        	break;
                     }
-
+                ++i;
                 }
+            }
 
-            }
-            catch (Exception ex)
-            {
-                System.Console.WriteLine("Возможна ошибка преобразования строки в список команд!\nПояснения в следующем окне.");
-                System.Console.WriteLine(ex.ToString());
-                return;
-            }
         }
     }
 }
